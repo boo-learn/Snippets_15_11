1e5bfbc616c19074e882b86ab481b9320878501b
boo <booblegum42@gmail.com>
2021-11-16 23:52:11 -0800

    Practice 3 task

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -1,19 +1,6 @@
-"""Snippets URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from MainApp import views
 
@@ -21,5 +8,5 @@ urlpatterns = [
     path('', views.index_page),
     path('snippets/add', views.add_snippet_page),
     path('snippets/list', views.snippets_page),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
