fafe1fe92248ac0b6b7589a5ae0340e63ccb33cd
boo <booblegum42@gmail.com>
2021-11-17 04:27:16 -0800

    add ModelsForm

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,16 +2,12 @@ from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
+from MainApp.forms import SnippetForm
 
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
     return render(request, 'pages/index.html', context)
-
-
-def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
 
 
 def snippets_page(request):
@@ -33,3 +29,21 @@ def single_snippet_page(request, id):
         "snippet": snippet
     }
     return render(request, 'pages/snippet_page.html', context)
+
+
+def add_snippet_page(request):
+    if request.method == "GET":
+        form = SnippetForm()
+        context = {
+            'pagename': 'Добавление нового сниппета',
+            'form': form
+        }
+        return render(request, 'pages/add_snippet.html', context)
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets-list")
+        return render(request, 'add_snippet.html', {'form': form})
+
+
